e52c6d2b27a0556fb8bbb9e748141361ac6cbf2d
Numan Arshad <154283305+numan-arshad@users.noreply.github.com>
2025-05-07 03:21:13 +0500

    Update securedataencryption.py

--- Secure-data-encryption/securedataencryption.py
+++ Secure-data-encryption/securedataencryption.py
@@ -3,7 +3,7 @@ import hashlib
 import json
 import os
 import time
-from cryptography.fernet import Fernet
+# from cryptography.fernet import Fernet
 from base64 import urlsafe_b64encode
 from hashlib import pbkdf2_hmac
 
